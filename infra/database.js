96001c20bca43dc1d98dfdb8e35b2a2a13691550
yurigm1 <yurigm1@gmail.com>
2025-03-06 21:06:12 +0000

    Make  more robust and updated  for  API

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,15 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "developer" ? false : true,
   });
+  // console.log("Credenciais do Postgres:", {
+  //   host: process.env.POSTGRES_HOST,
+  //   port: process.env.POSTGRES_PORT,
+  //   user: process.env.POSTGRES_USER,
+  //   database: process.env.POSTGRES_DB,
+  //   password: process.env.POSTGRES_PASSWORD,
+  // });
   await client.connect();
   try {
     const result = await client.query(queryObject);

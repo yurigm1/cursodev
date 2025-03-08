d92473443256337f08725919d6d476838f5e9a2a
yurigm1 <yurigm1@gmail.com>
2025-03-08 15:22:16 +0000

    Adds `/api/v1/migrations` endpoint

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const detenv = require("dotenv");
+detenv.config({
+  path: ".env.development",
+});
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: ".",
+});
+
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>"],
+});
+
+module.exports = jestConfig;
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,42 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  try {
+    if (!request.method === "GET" && !request.method === "POST") {
+      return response.status(405).end();
+    }
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return response.status(200).json(pendingMigrations);
+    } else if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch {
+    console.log(error);
+  } finally {
+    dbClient.end();
+  }
+}

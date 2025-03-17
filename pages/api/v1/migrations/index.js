e0a50a51d30bb038a5078f7d34a8f240c7010ebf
yurigm1 <yurigm1@gmail.com>
2025-03-17 18:39:03 +0000

    Created return of not allowed methods on `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,18 +3,25 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({ error: `Method "${request.method}" now allowed` });
+  }
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  let dbClient;
 
   try {
+    dbClient = await database.getNewClient();
+
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
@@ -28,11 +35,10 @@ export default async function migrations(request, response) {
         return response.status(201).json(migratedMigrations);
       }
       return response.status(200).json(migratedMigrations);
-    } else {
-      return response.status(405).end(`Method ${request.method} now allowed`);
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    throw error;
   } finally {
     await dbClient.end();
   }

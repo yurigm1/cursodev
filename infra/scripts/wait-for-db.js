e6c7fc73c76e02af517a653dd8bf6ee31ac887b3
yurigm1 <yurigm1@gmail.com>
2025-03-17 19:19:47 +0000

    adds `wait-for-db` script

---
+++ infra/scripts/wait-for-db.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkDB() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkDB();
+      return;
+    }
+    console.log("\n🟢 DB aceitando conexões\n");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando DB aceitar conexões");
+
+checkDB();

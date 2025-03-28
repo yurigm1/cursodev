189a7f982ceddd4403df0ce3fdeb9b27cccf9404
yurigm1 <yurigm1@gmail.com>
2025-03-28 16:17:02 +0000

    adds `Automated Tests` GitHub Action

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -15,6 +15,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toContain("16.");
   expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(10);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
-
-  console.log(responseBody);
 });
